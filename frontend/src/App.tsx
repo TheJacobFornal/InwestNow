a1fcf88b4a6e7fa5305f8866b51d8740803166d2
Kuba <jk.fornal@gmail.com>
2025-08-17 19:11:15 +0200

    remove .env>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -62,7 +62,7 @@ function App() {
         </a>
       </div>
 
-      <h1>Vite + React</h1>
+      <h1>Vite + React + Szef</h1>
 
       <div className="card">
         <button onClick={() => setCount((c) => c + 1)}>
