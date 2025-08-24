a794fd3198809cec24e548d4631da60ad91cb7d5
Kuba <jk.fornal@gmail.com>
2025-08-24 13:23:43 +0200

    db tests

--- backend/app.py
+++ backend/app.py
@@ -19,7 +19,7 @@ app = FastAPI(title="Company API")
 # Allow your Vite dev server to call this API
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://www.mycoins.pl"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
